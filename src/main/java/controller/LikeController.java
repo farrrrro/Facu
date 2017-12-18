f786eb6b16cacfbfd3ab828452f5d30a09d9e995
farrrrro <fabriciogardini@gmail.com>
2017-12-18 04:00:53 -0300

    Update LikeController.java
    Agrego un par de lineas para evaluacion, lo que ves ahi es mas o menos parecido a la actualizacion del password del usuario. Utilizando los mismos if ya vamos haciendo el conteo de los likes de los usuarios. NO QUITAR EL CODIGO ANTERIOR ----> esto lo digo porque vamos a perder trackeo de la cantidad de usuarios que ponen like y eso nos sirve para el control. Es mas barato ir metiendolo en la variable del post el conteo.

--- src/main/java/controller/LikeController.java
+++ src/main/java/controller/LikeController.java
@@ -22,12 +22,18 @@ public class LikeController {
 				Likes.class);
 		q.setParameter("user", user);
 		q.setParameter("post", post);
+		entityManager.getTransaction().begin();
 		if (q.getResultList().equals(null)) {
 			entityManager.persist(l);
+			String hql = "UPDATE Post SET likes=likes + 1 WHERE post=?1";
 		} else {
 			entityManager.remove(q.getSingleResult());
 			entityManager.persist(l);
+			String hql = "UPDATE Post SET likes=likes - 1 WHERE post=?1";
 		}
+		int executeUpdate = entityManager.createQuery(hql).setParameter(1, post).executeUpdate();
+		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 	
 	public int amount(Post post) {
